cc8bfc033fef3c0aeeb9741d89bfecf58edc48bf
Wilson Henao <wilson.henao@utp.edu.co>
2023-01-12 17:49:33 -0500

    Configuracion inicial

---
+++ gestor-de-notas/src/main/java/com/synapsesft/gestordenotas/GestorDeNotasApplication.java
@@ -0,0 +1,13 @@
+package com.synapsesft.gestordenotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDeNotasApplication.class, args);
+	}
+
+}
---
+++ gestor-de-notas/src/test/java/com/synapsesft/gestordenotas/GestorDeNotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synapsesft.gestordenotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDeNotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
